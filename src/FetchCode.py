a22f4fd26ebff8024d0c316c18b6460b87256f67
shawn <tjushengyang@hotmail.com>
2018-06-01 01:23:30 +0800

    dev

--- src/FetchCode.py
+++ src/FetchCode.py
@@ -3,13 +3,17 @@
 import git
 import sys
 import os
-def copy_file(src_branch,dst_branch,source_path = r'D:\project\test1'):
-    if(os.path.exists(source_path) and os.path.isdir(source_path)):
+def copy_file(old_branch,new_branch,src_path,dst_path):
+    if(os.path.exists(src_path) and os.path.isdir(src_path)):
+        pass
+    else:
+        return -1
+    if(os.path.exists(dst_path) and os.path.isdir(dst_path)):
         pass
     else:
         return -1
     try:
-        repo = git.Repo(source_path)
+        repo = git.Repo(src_path)
     except git.InvalidGitRepositoryError as e:
         print("path %s is not a repo" % e)
         return -1
@@ -18,8 +22,13 @@ def copy_file(src_branch,dst_branch,source_path = r'D:\project\test1'):
     else:
         git_oper = repo.git
         git_oper.fetch() 
-        print(git_oper.diff('--name-only',src_branch,dst_branch))    
+        query_result = git_oper.diff('--name-only',old_branch,new_branch)
+        rel_file_list = query_result.split('\n')
+        abs_file_list = []
+        for item in rel_file_list:
+            abs_file_list.append(os.path.join(dst_path,item.replace('/','\\')))
+        print(abs_file_list)
+        
+    return 0
 
-
-
-print(copy_file('HEAD','HEAD^'))
+print(copy_file('HEAD','HEAD^','D:\\project\\test1','D:\\'))

f8b30b4c1f037151e5825578debb462d86a1c506
shawn <tjushengyang@hotmail.com>
2018-06-03 19:53:39 +0800

    frame

--- src/Frame.py
+++ src/Frame.py
@@ -9,6 +9,7 @@
 
 import wx
 import wx.xrc
+import repo_conf
 
 ###########################################################################
 ## Class MyFrame1
@@ -18,6 +19,10 @@ class MyFrame1 ( wx.Frame ):
 	
 	def __init__( self, parent ):
 		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"CodeClub Review Tool", pos = wx.DefaultPosition, size = wx.Size( 280,470 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		username    = repo_conf.get_option_value('remote','username')
+		password    = repo_conf.get_option_value('remote','password')
+		repo_path   = repo_conf.get_option_value('local','repository')
+		output_path = repo_conf.get_option_value('local','output')
 		
 		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
 		
@@ -31,7 +36,7 @@ class MyFrame1 ( wx.Frame ):
 		self.m_s_username.Wrap( -1 )
 		bSizer2.Add( self.m_s_username, 0, wx.ALL, 5 )
 		
-		self.m_username = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.Point( -1,-1 ), wx.Size( 200,-1 ), 0 )
+		self.m_username = wx.TextCtrl( self, wx.ID_ANY, username, wx.Point( -1,-1 ), wx.Size( 200,-1 ), 0 )
 		self.m_username.SetFont( wx.Font( wx.NORMAL_FONT.GetPointSize(), 70, 90, 90, False, wx.EmptyString ) )
 		
 		bSizer2.Add( self.m_username, 0, wx.ALL, 5 )
@@ -40,21 +45,21 @@ class MyFrame1 ( wx.Frame ):
 		self.m_s_password.Wrap( -1 )
 		bSizer2.Add( self.m_s_password, 0, wx.ALL, 5 )
 		
-		self.m_password = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 200,-1 ), wx.TE_PASSWORD )
+		self.m_password = wx.TextCtrl( self, wx.ID_ANY, password, wx.DefaultPosition, wx.Size( 200,-1 ), wx.TE_PASSWORD )
 		bSizer2.Add( self.m_password, 0, wx.ALL, 5 )
 		
 		self.m_s_rep_path = wx.StaticText( self, wx.ID_ANY, u"Repository Path", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_s_rep_path.Wrap( -1 )
 		bSizer2.Add( self.m_s_rep_path, 0, wx.ALL, 5 )
 		
-		self.m_rep_path = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 200,-1 ), 0 )
+		self.m_rep_path = wx.TextCtrl( self, wx.ID_ANY, repo_path, wx.DefaultPosition, wx.Size( 200,-1 ), 0 )
 		bSizer2.Add( self.m_rep_path, 0, wx.ALL, 5 )
 		
 		self.m_s_output_path = wx.StaticText( self, wx.ID_ANY, u"Output Path", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_s_output_path.Wrap( -1 )
 		bSizer2.Add( self.m_s_output_path, 0, wx.ALL, 5 )
 		
-		self.m_output_path = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 200,-1 ), 0 )
+		self.m_output_path = wx.TextCtrl( self, wx.ID_ANY, output_path, wx.DefaultPosition, wx.Size( 200,-1 ), 0 )
 		bSizer2.Add( self.m_output_path, 0, wx.ALL, 5 )
 		
 		self.m_generate = wx.Button( self, wx.ID_ANY, u"Generate", wx.DefaultPosition, wx.DefaultSize, 0 )
